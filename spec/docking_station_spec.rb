60eedb87806aa350801f1793504f8888732000c3
Max Provin <maxprovin@outlook.com>
2016-04-14 16:11:46 +0100

    Step 19 - isolating tests with doubles

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,10 @@
 require 'docking_station'
 
 describe DockingStation do
-let(:bike) { Bike.new }
-let(:bikes) { subject.bikes }
-  it 'has a DEFAULT_CAPACITY unless capacity is specified' do
-    expect subject.capacity == DockingStation::DEFAULT_CAPACITY
+let(:bike) { double(:bike) } # dependency
+let(:bikes) { subject.bikes } # OK!
+  it 'has a DEFAULT_CAPACITY unless capacity is specified' do    #ok
+    expect subject.capacity == DockingStation::DEFAULT_CAPACITY  #ok
   end
 
 
@@ -21,43 +21,43 @@ describe 'initialization' do
 
   it 'defaults capacity' do
     described_class::DEFAULT_CAPACITY.times do
-      subject.dock(bike)
+      subject.dock(bike) # dependency
     end
-    expect{ subject.dock(bike) }.to raise_error 'Docking station full'
+    expect{ subject.dock(bike) }.to raise_error 'Docking station full' # depend
   end
 end
 
-  it { is_expected.to respond_to :release_bike }
+  it { is_expected.to respond_to :release_bike } # ok
 
    describe '#release_bike' do
      it 'raises an error if there is no bike' do
       (expect { subject.release_bike }.to raise_error 'No bikes available') if subject.bikes.length == 0
-      end
+      end # all ok!
 
      it 'raises an error if bike isn\'t working' do
-     bike.report(false)
-     subject.dock(bike)
-     (expect { subject.release_bike }.to raise_exception 'Bike not working') unless bikes.last.working?
-     end
+     bike.report(false)  # depend
+     subject.dock(bike)  # depend
+     (expect { subject.release_bike }.to raise_exception 'Bike not working') #unless bikes.last.working?
+     end   # sort of possible dependency or something
   end
 
 
 
 
     it 'releases a bike' do
-      subject.dock(bike)
-      expect(subject.release_bike).to eq bike
+      subject.dock(bike)   # depend
+      expect(subject.release_bike).to eq bike # depend
     end
 
      it 'releases a working bike' do
-       subject.dock(bike)
+       subject.dock(bike)   # depend
       # bike = subject.release_bike
     #   expect(bike).to be_working
-       expect(subject.release_bike).to be_working
+       expect(subject.release_bike).to be_working # maybe depend probably faily
       end
 
 
-  it { is_expected.to respond_to(:dock).with(1).argument }
+  it { is_expected.to respond_to(:dock).with(1).argument }  #ok
 
   describe '#dock' do
     it 'raises an error if there is already a bike docked' do
@@ -65,24 +65,22 @@ end
       #bike2 = Bike.new
       #subject.dock(bike)
       #expect { subject.dock(bike2) }.to raise_error 'Bike already docked!'
-      subject.capacity.times { subject.dock bike }
-      expect { subject.dock bike }.to raise_error 'Docking station full'
+      subject.capacity.times { subject.dock bike }   # depend
+      expect { subject.dock bike }.to raise_error 'Docking station full'  #depend
     end
   end
 
 
 
   it 'docks bike' do
-    expect(subject.dock(bike)).to eq bike
+    expect(subject.dock(bike)).to eq bike  #depend
   end
 
   it 'returns docked bike' do
-    subject.dock(bike)
-    expect(subject.bikes.last).to eq bike
+    subject.dock(bike)      #depend
+    expect(subject.bikes.last).to eq bike # depend
   end
 
-  it { is_expected.to respond_to(:bikes) }
-
-
+  it { is_expected.to respond_to(:bikes) } # ok
 
 end
